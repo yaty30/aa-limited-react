2099cad67374da48a9cea9dbfc1197b7af071699
James <josueyat318@gmail.com>
2024-12-20 00:06:52 +0800

    hero content responsive

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,14 +1,26 @@
 import React from "react";
-import { Box, Button, Grid2 as Grid } from "@mui/material";
+import { Box, Button, Grid2 as Grid, useMediaQuery } from "@mui/material";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import { observer } from "mobx-react-lite";
 import colors from "../../styles/colors";
+import { useTheme } from "@mui/material/styles";
 
 import Typography from "../ui/Typography";
 
 const backgroundImage = "/assets/images/Banner Background Image.png";
 
 export default observer(() => {
+  const theme = useTheme();
+  const xss = useMediaQuery(theme.breakpoints.down("md"));
+  const sms = useMediaQuery(theme.breakpoints.down("md"));
+  const mds = useMediaQuery(theme.breakpoints.down("md"));
+  const lgs = useMediaQuery(theme.breakpoints.down("md"));
+  const xls = useMediaQuery(theme.breakpoints.down("md"));
+
+  const ismd = (mdstyle, other) => {
+    return mds || lgs || xls ? mdstyle : other;
+  };
+
   return (
     <Box
       sx={{
@@ -38,17 +50,20 @@ export default observer(() => {
           <Grid item size={{ xs: 12 }} className="hero-text-fade-in">
             <Box sx={{ maxWidth: 640 }}>
               <Box sx={{ my: 1 }}>
-                <Typography variant="subtitle">
+                <Typography variant={ismd("label2", "subtitle")}>
                   Welcome to the AIO Study Platform
                 </Typography>
               </Box>
               <Box sx={{ my: 1 }}>
-                <Typography variant="heading1" color={colors.primary[700]}>
+                <Typography
+                  variant={ismd("heading3", "heading1")}
+                  color={colors.primary[700]}
+                >
                   All-in-One Platform for Self-Learners
                 </Typography>
               </Box>
               <Box sx={{ my: 1 }}>
-                <Typography variant="body1">
+                <Typography variant={ismd("caption1", "body1")}>
                   Lorem ipsum dolor sit amet consectetur. Pellentesque velit id
                   sodales et at fermentum. Sed id egestas nec odio sit.
                 </Typography>
@@ -57,6 +72,7 @@ export default observer(() => {
               <Box sx={{ my: 2 }}>
                 <Button
                   variant="contained"
+                  size={ismd("small", "large")}
                   endIcon={<ArrowForwardIcon />}
                   sx={{
                     textTransform: "capitalize",
