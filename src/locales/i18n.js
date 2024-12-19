a1f713cc860344f24a597c2c061d69759dcfa9aa
James <josueyat318@gmail.com>
2024-12-19 23:35:51 +0800

    update

--- src/locales/i18n.js
+++ src/locales/i18n.js
@@ -1,34 +1,34 @@
-// import i18n from "i18next";
-// import { initReactI18next } from "react-i18next";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
 
-// // Translation resources
-// const resources = {
-//   en: {
-//     translation: {
-//       home: "Home",
-//       aboutUs: "About Us",
-//     },
-//   },
-//   zh: {
-//     translation: {
-//       home: "首頁",
-//       aboutUs: "關於我們",
-//     },
-//   },
-//   sm_zh: {
-//     translation: {
-//       home: "首頁",
-//       aboutUs: "關於我們",
-//     },
-//   },
-// };
+// Translation resources
+const resources = {
+  en: {
+    translation: {
+      home: "Home",
+      aboutUs: "About Us",
+    },
+  },
+  zh: {
+    translation: {
+      home: "首頁",
+      aboutUs: "關於我們",
+    },
+  },
+  sm_zh: {
+    translation: {
+      home: "首頁",
+      aboutUs: "關於我們",
+    },
+  },
+};
 
-// i18n.use(initReactI18next).init({
-//   resources,
-//   fallbackLng: "en",
-//   interpolation: {
-//     escapeValue: false,
-//   },
-// });
+i18n.use(initReactI18next).init({
+  resources,
+  fallbackLng: "en",
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
-// export default i18n;
+export default i18n;
